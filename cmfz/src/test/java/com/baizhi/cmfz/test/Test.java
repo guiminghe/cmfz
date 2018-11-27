5c1e6e2febbedde03505379c4b4bb37b661ff000
guiminghe <865318553@qq.com>
2018-11-27 15:30:55 +0800

    3

---
+++ cmfz/src/test/java/com/baizhi/cmfz/test/Test.java
@@ -0,0 +1,14 @@
+package com.baizhi.cmfz.test;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Test {
+    @org.junit.Test
+    public void test(){
+        System.out.println("test");
+    }
+}
